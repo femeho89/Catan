8801909f59cd3b35e4c7c5502298b298a306e208
femeho89 <feltseh@outlook.com>
2025-12-21 12:11:59 +0100

    Ik heb een nieuw bestand toegevoegd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.io.File;
+import java.util.*;
+
 public class Main {
 
+    public static String file1 = "Catan";
+
     public static void main(String[] args) {
+
+
+        File file = new File(file1);
+
+
     }
 }

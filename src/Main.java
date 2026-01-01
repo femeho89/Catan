25d66a42493e77ed5b06569519306230696d03e5
femeho89 <feltseh@outlook.com>
2026-01-01 16:12:07 +0100

    Made the screen update

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,20 @@ public class Main {
         JFrame frame = new JFrame(file1);    // makes the frame for the game
         frame.setSize(1366, 720);     // sets the frame size
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    // makes the game close when the cross is pressed
-        frame.setVisible(true);     // makes the frame visible
 
-        Color colorBackground = new Color(255, 255, 255);    // makes a white color
-        frame.getContentPane().setBackground(colorBackground);
+        while(playing) {
 
-        ImageIcon cardKnight = new ImageIcon("Kaart Ridder.png");
-        JLabel knight = new JLabel(cardKnight);
+            Color colorBackground = new Color(255, 255, 255);    // makes a white color
+            frame.getContentPane().setBackground(colorBackground);
 
-        knight.setBounds(100, 100, cardKnight.getIconWidth(), cardKnight.getIconHeight());
-        frame.add(knight);
-        frame.setVisible(true);
+            ImageIcon cardKnight = new ImageIcon("Kaart Ridder.png");
+            JLabel knight = new JLabel(cardKnight);
 
+            knight.setBounds(100, 100, cardKnight.getIconWidth(), cardKnight.getIconHeight());
+            frame.add(knight);
+            frame.setVisible(true);     // makes the frame visible
+
+            frame.revalidate();
+        }
     }
 }

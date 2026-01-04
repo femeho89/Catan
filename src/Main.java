d1a2829dc5f55534e8e34e58930d61f21f9328b8
femeho89 <feltseh@outlook.com>
2026-01-04 13:10:24 +0100

    Heb de kaart laten bewegen.

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,31 @@ import java.awt.*;
 public class Main {
 
     public static String file1 = "Catan";
-    public static boolean playing = true;
+    public static int xKnight = 100;
 
     public static void main(String[] args) {
 
         JFrame frame = new JFrame(file1);    // makes the frame for the game
         frame.setSize(1366, 720);     // sets the frame size
 
-        while(playing) {
+        ImageIcon cardKnight = new ImageIcon("Kaart Ridder.png");
+        JLabel knight = new JLabel(cardKnight);
+        frame.setLayout(null);
 
-            Color colorBackground = new Color(255, 255, 255);    // makes a white color
-            frame.getContentPane().setBackground(colorBackground);
+        knight.setBounds(xKnight, 100, cardKnight.getIconWidth(), cardKnight.getIconHeight());
+        frame.add(knight);
 
-            ImageIcon cardKnight = new ImageIcon("Kaart Ridder.png");
-            JLabel knight = new JLabel(cardKnight);
+        Timer timerKnight = new Timer(9, e -> {
+            xKnight += 3;
+            knight.setLocation(xKnight, 100);
+        });
 
-            knight.setBounds(100, 100, cardKnight.getIconWidth(), cardKnight.getIconHeight());
-            frame.add(knight);
+        timerKnight.start();
 
-            frame.setVisible(true);     // makes the frame visible
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    // makes the game close when the cross is pressed
-            frame.revalidate();
-            frame.repaint();
-        }
+        Color colorBackground = new Color(255, 255, 255);    // makes a white color
+        frame.getContentPane().setBackground(colorBackground);
+
+        frame.setVisible(true);     // makes the frame visible
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    // makes the game close when the cross is pressed
     }
 }

40f7173466e5d28c322ff06c769a56a91d52ba56
femeho89 <feltseh@outlook.com>
2026-01-09 15:38:27 +0100

    Ik heb een startscherm gemaakt.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class Main {
 
@@ -14,37 +13,52 @@ public class Main {
 
         JFrame frame = new JFrame(file1);    // makes the frame for the game
         frame.setSize(1366, 720);     // sets the frame size
+        frame.setLayout(null);
 
         ImageIcon cardKnight = new ImageIcon("Kaart Ridder.png");
         JLabel knight = new JLabel(cardKnight);
-        frame.setLayout(null);
 
         JButton startButton = new JButton("Start Catan");
         startButton.setBounds(100, 100, 200, 50);
         frame.add(startButton);
 
-        ActionListener listener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent f) {
-                JFrame playingFrame = new JFrame(file1);    //The playing screen
-                playingFrame.setSize(1366, 720);
-                playingFrame.setLayout(null);
-                knight.setBounds(xKnight, yKnight, cardKnight.getIconWidth(), cardKnight.getIconHeight());
-                playingFrame.add(knight);
+        ActionListener listener = f -> {
+            JFrame playingFrame = new JFrame(file1);    //The playing screen
+            playingFrame.setSize(1366, 720);
+            playingFrame.setLayout(null);
+            knight.setBounds(xKnight, yKnight, cardKnight.getIconWidth(), cardKnight.getIconHeight());
+            playingFrame.add(knight);
 
-                Timer timerKnight = new Timer(9, e -> {
-                    xKnight += (3 * direccion);
-                    if(xKnight + cardKnight.getIconWidth() >= frame.getContentPane().getWidth() || xKnight <= 0) {
-                        direccion *= -1;
+            Timer timerKnight = new Timer(9, e -> {
+                xKnight += (3 * direccion);
+                if(xKnight + cardKnight.getIconWidth() >= playingFrame.getContentPane().getWidth() || xKnight <= 0) {
+                    direccion *= -1;
+                }
+                knight.setLocation(xKnight, yKnight);
+            });
+
+            timerKnight.start();
+
+            frame.setVisible(false);
+            playingFrame.setVisible(true);
+
+            playingFrame.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    super.mouseClicked(e);
+
+                    if(SwingUtilities.isRightMouseButton(e)) {
+                        int mouseX = e.getX();
+                        int mouseY = e.getY();
+
+                        if (mouseX <= xKnight + cardKnight.getIconWidth() && mouseX >= xKnight && mouseY <= yKnight + cardKnight.getIconHeight() && mouseY >= yKnight) {    //Makes the card disappear when the right mouse button is pressed
+                            knight.setVisible(false);
+                        }
                     }
-                    knight.setLocation(xKnight, yKnight);
-                });
+                }
+            });
 
-                timerKnight.start();
-
-                frame.setVisible(false);
-                playingFrame.setVisible(true);
-            }
+            playingFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         };
 
         startButton.addActionListener(listener);

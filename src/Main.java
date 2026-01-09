da53a29e5e354ea9ffa2aacec78f6c586761b850
femeho89 <feltseh@outlook.com>
2026-01-09 11:43:12 +0100

    Ik heb een startscherm gemaakt

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
 
     public static String file1 = "Catan";
     public static int xKnight = 100;
+    public static int yKnight = 100;
+    public static int direccion = 1;
 
     public static void main(String[] args) {
 
@@ -15,15 +19,35 @@ public class Main {
         JLabel knight = new JLabel(cardKnight);
         frame.setLayout(null);
 
-        knight.setBounds(xKnight, 100, cardKnight.getIconWidth(), cardKnight.getIconHeight());
-        frame.add(knight);
+        JButton startButton = new JButton("Start Catan");
+        startButton.setBounds(100, 100, 200, 50);
+        frame.add(startButton);
 
-        Timer timerKnight = new Timer(9, e -> {
-            xKnight += 3;
-            knight.setLocation(xKnight, 100);
-        });
+        ActionListener listener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent f) {
+                JFrame playingFrame = new JFrame(file1);    //The playing screen
+                playingFrame.setSize(1366, 720);
+                playingFrame.setLayout(null);
+                knight.setBounds(xKnight, yKnight, cardKnight.getIconWidth(), cardKnight.getIconHeight());
+                playingFrame.add(knight);
 
-        timerKnight.start();
+                Timer timerKnight = new Timer(9, e -> {
+                    xKnight += (3 * direccion);
+                    if(xKnight + cardKnight.getIconWidth() >= frame.getContentPane().getWidth() || xKnight <= 0) {
+                        direccion *= -1;
+                    }
+                    knight.setLocation(xKnight, yKnight);
+                });
+
+                timerKnight.start();
+
+                frame.setVisible(false);
+                playingFrame.setVisible(true);
+            }
+        };
+
+        startButton.addActionListener(listener);
 
         Color colorBackground = new Color(255, 255, 255);    // makes a white color
         frame.getContentPane().setBackground(colorBackground);

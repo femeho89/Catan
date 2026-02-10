2108751b965f25975de484123b2739de3c9f432e
femeho89 <feltseh@outlook.com>
2026-02-10 12:51:26 +0100

    Ervoor gezorgd dat de kaarten stoppen als je naar de markt gaat.

--- src/Market.java
+++
@@ -1,6 +0,0 @@
-public class Market {
-    private final Kaart card3;
-    public Market(){
-        card3 = new Kaart(0, 100, cardImage.getIconWidth(), cardImage.getIconHeight(), Kaart.Type.TOWN, cardImage);
-    }
-}
--- src/MarketActionListener.java
+++ src/MarketActionListener.java
@@ -9,21 +9,27 @@ public class MarketActionListener implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         StartButtonActionListener.timer.stop();
+
         JFrame marketFrame = new JFrame(Main.file1);
         marketFrame.setSize(1366,720);
         marketFrame.setLayout(null);
         StartButtonActionListener.getPlayingFrame().setVisible(false);
         marketFrame.setVisible(true);
+
+        JLabel grondstofKaart = new JLabel()
+
         ActionListener marketBackListener = back -> {
             StartButtonActionListener.getPlayingFrame().setVisible(true);
             marketFrame.setVisible(false);
             StartButtonActionListener.timer.start();
         };
+
         JButton marketExitButton = new JButton("Terug naar het spel!");
         marketExitButton.setBounds(50, 550, 200, 50);
         marketExitButton.setBackground(new Color(200, 0, 0));
         marketFrame.add(marketExitButton);
         marketExitButton.addActionListener(marketBackListener);
+
         marketFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }

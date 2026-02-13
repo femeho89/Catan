96161efd1dfa3d2cff98ac6267f80e7c5077b724
Robin-Sierra <Jensediependaal@icloud.com>
2026-02-13 19:52:47 +0100

    working on winning

--- src/Win.java
+++ src/Win.java
@@ -1,8 +1,29 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class Win {
     //makes a win frame for when a player has won the game
-    private static JFrame winFrame;
+    private static JFrame winFrame = new JFrame(Main.file1);
+    public void actionPerformed(ActionEvent e){
+        //sets the size of GUI component winFrame
+        winFrame.setSize(1366, 720);
+        //sets the layout of GUI component winFrame
+        winFrame.setLayout(null);
+        //sets background colour of GUI component winFrame
+        winFrame.setBackground(new Color(200, 50, 150));
+        //sets playingFrame to invisible
+        StartButtonActionListener.getPlayingFrame().setVisible(false);
+        //sets winFrame to visible
+        winFrame.setVisible(true);
+
+        JTextArea winText = new JTextArea("""
+                Gefeliciteer!!! \s
+                Je hebt Catan gewonnen!!!""");
+        winText.setEditable(false);
+        winText.setWrapStyleWord(true);
+        winText.setBackground(new Color(255,50,150));
+    }
 
     public static JFrame getWinFrame() {
         return winFrame;

0e173a54a6f3aa37ecb66137edecd902ff39ae7b
Robin-Sierra <Jensediependaal@icloud.com>
2026-02-14 19:53:42 +0100

    bouwkaarten bouwen, "Wat wil je bouwen?"

--- src/Bouwen.java
+++ src/Bouwen.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -7,11 +8,21 @@ public class Bouwen implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             StartButtonActionListener.timer.stop();
 
-            JFrame BuildFrame = new JFrame(Main.file1);
-            BuildFrame.setSize(1366, 720);
-            BuildFrame.setLayout(null);
+            JFrame buildFrame = new JFrame(Main.file1);
+            buildFrame.setSize(1366, 720);
+            buildFrame.setLayout(null);
+            buildFrame.getContentPane().setBackground(new Color(237, 140, 98));
             StartButtonActionListener.getPlayingFrame().setVisible(false);
-            BuildFrame.setVisible(true);
+            buildFrame.setVisible(true);
+
+            //initialises question Label & declares it a new JLabel with the text "wat wil je bouwen?"
+            JLabel questionLabel = new JLabel("wat wil je bouwen?");
+            //sets the position & size of GUI component questionLabel
+            questionLabel.setBounds(633, 100, 200, 50);
+            //sets font for questionLabel
+            questionLabel.setFont(new Font("Arial", Font.BOLD, 16));
+            //adds questionLabel to frame
+            buildFrame.add(questionLabel);
         }
     }
 

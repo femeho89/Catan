76d594c7ae7bb400288447029815110100e6cd95
Robin-Sierra <Jensediependaal@icloud.com>
2026-02-15 21:26:17 +0100

    working on bouwkaarten bouwen,

--- src/Bouwen.java
+++ src/Bouwen.java
@@ -4,10 +4,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Bouwen implements ActionListener {
-    static ImageIcon bouwStraatKaart = new ImageIcon("Kaart Straat.png");
+    static ImageIcon bouwStraatKaart = new ImageIcon("Straat Kaart catan 2.png");
     BouwKaart StraatBouwenKaart = new BouwKaart(100, 100, bouwStraatKaart.getIconHeight(), bouwStraatKaart.getIconWidth(), bouwStraatKaart, BouwKaart.BouwType.STREET);
+    static ImageIcon bouwDorpKaart = new ImageIcon("Kaart Dorp.png");
+    BouwKaart DorpBouwenKaart = new BouwKaart(100, 100, bouwDorpKaart.getIconHeight(), bouwDorpKaart.getIconWidth(), bouwDorpKaart, BouwKaart.BouwType.TOWN);
+    static ImageIcon bouwStadKaart = new ImageIcon("Kaart Stad.png");
+    BouwKaart StadBouwenKaart = new BouwKaart(100, 100, bouwStadKaart.getIconHeight(), bouwStadKaart.getIconWidth(), bouwStadKaart, BouwKaart.BouwType.CITY);
+    static ImageIcon bouwStadsUitbreidingKaart = new ImageIcon("Stadsuitbreiding Kaart catan2.png");
+    BouwKaart StadsuitbreidingBouwenKaart = new BouwKaart(100, 100, bouwStadsUitbreidingKaart.getIconHeight(), bouwStadsUitbreidingKaart.getIconWidth(), bouwStadsUitbreidingKaart, BouwKaart.BouwType.CITY);
 
-        @Override
+
+    @Override
         public void actionPerformed(ActionEvent e) {
             StartButtonActionListener.timer.stop();
 
@@ -30,12 +37,45 @@ public class Bouwen implements ActionListener {
             ActionListener straatBuildListener = event -> {
                 StartButtonActionListener.getPlayingFrame().add(StraatBouwenKaart);
             };
+            ActionListener dorpBuildListener = event -> {
+               StartButtonActionListener.getPlayingFrame().add(DorpBouwenKaart);
+            };
+            ActionListener stadBuildListener = event -> {
+                StartButtonActionListener.getPlayingFrame().add(StadBouwenKaart);
+            };
+            ActionListener stadsuitbreidingBuildListener = event -> {
+                StartButtonActionListener.getPlayingFrame().add(StadsuitbreidingBouwenKaart);
+            };
 
-            JButton straatBuildCard = new JButton((Icon) StraatBouwenKaart);
-            straatBuildCard.setBounds(60, 600, bouwStraatKaart.getIconHeight(), bouwStraatKaart.getIconWidth());
+            //trying to get it so that players can press on the card they wanna build
+
+            //Straat
+            JButton straatBuildCard = new JButton(bouwStraatKaart);
+            straatBuildCard.setBounds(50, 300, bouwStraatKaart.getIconHeight(), bouwStraatKaart.getIconWidth());
             buildFrame.add(straatBuildCard);
             straatBuildCard.setVisible(true);
             straatBuildCard.addActionListener(straatBuildListener);
+
+            //Dorp
+            JButton dorpBuildCard = new JButton(bouwDorpKaart);
+            dorpBuildCard.setBounds(250, 300, bouwDorpKaart.getIconHeight(), bouwDorpKaart.getIconWidth());
+            buildFrame.add(dorpBuildCard);
+            dorpBuildCard.setVisible(true);
+            dorpBuildCard.addActionListener(dorpBuildListener);
+
+            //Stad
+            JButton stadBuildCard = new JButton(bouwStadKaart);
+            stadBuildCard.setBounds(500, 300, bouwStadKaart.getIconHeight(), bouwStadKaart.getIconWidth());
+            buildFrame.add(stadBuildCard);
+            stadBuildCard.setVisible(true);
+            stadBuildCard.addActionListener(stadBuildListener);
+
+            //Stadsuitbreiding
+            JButton stadsuitbreidingBuildCard = new JButton(bouwStadsUitbreidingKaart);
+            stadsuitbreidingBuildCard.setBounds(750, 300, bouwStadsUitbreidingKaart.getIconHeight(), bouwStadsUitbreidingKaart.getIconWidth());
+            buildFrame.add(stadsuitbreidingBuildCard);
+            stadsuitbreidingBuildCard.setVisible(true);
+            stadsuitbreidingBuildCard.addActionListener(stadsuitbreidingBuildListener);
 
             ActionListener buildBackListener = event -> {
                 StartButtonActionListener.getPlayingFrame().setVisible(true);
